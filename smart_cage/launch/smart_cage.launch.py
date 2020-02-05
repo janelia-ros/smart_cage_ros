700a2fa06ed1ac3da52ac25f67488a1e95ee4b0d
Peter Polidoro <peterpolidoro@gmail.com>
2020-02-05 10:39:30 -0500

    Add smart_cage

---
+++ smart_cage/launch/smart_cage.launch.py
@@ -0,0 +1,22 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'node_prefix',
+            default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
+            description='Prefix for node names'),
+        launch_ros.actions.Node(
+            package='lickport', node_executable='lickport_node', output='screen',
+            node_name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'lickport_node']),
+        launch_ros.actions.Node(
+            package='tunnel', node_executable='tunnel_node', output='screen',
+            node_name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'tunnel_node']),
+        launch_ros.actions.Node(
+            package='smart_cage_data_writer', node_executable='tunnel_data_writer_node', output='screen',
+            node_name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'tunnel_data_writer_node']),
+    ])
